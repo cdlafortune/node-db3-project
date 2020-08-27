a1f0a119ad41126883d7038116fd27ce1aa0db64
cdsuddreth <cdsuddreth@gmail.com>
2020-08-26 20:45:02 -0400

    finished queries

--- queries.sql
+++ queries.sql
@@ -2,8 +2,25 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT Product.ProductName, Category.CategoryName from Product
+JOIN Category on Product.CategoryId = Category.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+SELECT o.Id, s.CompanyName
+from 'Order' as o
+JOIN Shipper as s on o.ShipVia = s.Id
+WHERE o.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT p.ProductName, o.Quantity from OrderDetail as o
+JOIN 'Product' as p on o.ProductId = p.Id
+where o.OrderId = 10251;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT o.Id, c.CompanyName, e.LastName from 'Order' as o
+JOIN Customer as c on o.CustomerId = c.Id
+JOIN Employee as e on o.EmployeeId = e.Id
+ORDER by o.Id asc;
