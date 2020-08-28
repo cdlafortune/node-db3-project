8175d6595e2616de81efa7e5121a78ef3a037e9c
cdsuddreth <cdsuddreth@gmail.com>
2020-08-27 21:49:10 -0400

    MVP

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,18 +10,43 @@ function findById(id){
 
 function findSteps(id){
     return db('steps')
-        .join('schemes', 'steps.scheme_id', 'schemes.id')
-        .where({id})
-        .select('steps.id', 'steps.step_number', 'steps.instructions', 'scheme.scheme_name')
-        .orderBy('steps.step_number');
+        .join('schemes', 'schemes.id', 'steps.scheme_id')
+        .select('schemes.id', 'scheme_name', 'step_number', 'instructions')
+        .where('schemes.id', id)
+        .orderBy('step_number')
 };
 
-// function add(scheme){
-//     return db.
-// }
+function add(scheme){
+    return db('schemes')
+        .insert(scheme)
+        .then(id => {
+            return findById(id[0])
+        })
+        .catch(err => console.log(err))
+};
+
+
+function update(changes, id){
+    return db('schemes')
+        .where({id})
+        .update(changes)
+        .then(id => {
+            return findById(id[0])
+        })
+        .catch(err => console.log(err))
+};
+
+function remove(id){
+    return db('schemes')
+        .where({id})
+        .del()
+};
 
 module.exports = {
     find,
     findById,
-    findSteps
+    findSteps, 
+    add, 
+    update, 
+    remove
 };
